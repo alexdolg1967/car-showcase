86ef95410ac344d31e8300d47f6d5c3d191c1574
alexdolg1967 <alexdolg1967@mail.ru>
2023-08-01 13:29:03 +0300

    npm i @headlessui/react

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,17 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export {
   Hero,
   CustomButton,
+  SearchBar,
+  SearchManufacturer,
+  CustomFilter,
   Navbar,
   Footer
 }
